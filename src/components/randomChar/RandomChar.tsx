ffa15d55f13f8fc204734c5dd4f148a80aada22e
Aleksey Stolyarenko <astoliarenko@xbsoftware.by>
2023-05-02 13:22:44 +0300

    refactor: added object fit for not found img

--- src/components/randomChar/RandomChar.tsx
+++ src/components/randomChar/RandomChar.tsx
@@ -113,9 +113,16 @@ class RandomChar extends Component {
 const View = ({char}: any) => {
     const {name, description, thumbnail, homepage, wiki} = char;
 
+    let imgStyle = {objectFit : 'cover'};
+
+    if (thumbnail === 'http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available.jpg') {
+        imgStyle = {objectFit : 'unset'};
+    }
+
     return (
         <div className="randomchar__block">
-            <img src={thumbnail} alt="Random character" className="randomchar__img"/>
+            {/* @ts-ignore */}
+            <img src={thumbnail} alt="Random character" className="randomchar__img" style={imgStyle}/>
             <div className="randomchar__info">
                 <p className="randomchar__name">{name}</p>
                 <p className="randomchar__descr">{description}</p>
