69a7be3b8df6db65765f7ae4ddc30fb68e4b6e56
Aleksey Stolyarenko <astoliarenko@xbsoftware.by>
2023-05-04 10:55:28 +0300

    fix: changed mock View

--- src/components/charInfo/CharInfo.tsx
+++ src/components/charInfo/CharInfo.tsx
@@ -6,8 +6,9 @@ import Spinner from '../spinner/Spinner';
 import ErrorMessage from '../errorMessage/errorMessage';
 import Sceleton from '../skeleton/Skeleton';
 
+import { character } from '../interfaces/character';
+
 import './charInfo.scss';
-import thor from '../../resources/img/thor.jpeg';
 
 type charInfoProps = {
     charId: number | null
@@ -16,7 +17,7 @@ type charInfoProps = {
 type charInfoState = {
     loading: boolean,
     error: boolean,
-    char: any
+    char: character | null
 }
 
 class CharInfo extends Component<charInfoProps, charInfoState> {
@@ -30,9 +31,15 @@ class CharInfo extends Component<charInfoProps, charInfoState> {
         this.updateChar();
     }
 
+    componentDidUpdate(prevProps: Readonly<charInfoProps>, prevState: Readonly<charInfoState>, snapshot?: any): void {
+        if (this.props.charId !== prevProps.charId) {
+            this.updateChar();
+        }
+    }
+
     service = new MarvelService();
 
-    private updateChar() {
+    private updateChar = () => {
         const {charId} = this.props;
 
         if (!charId) {
@@ -46,20 +53,20 @@ class CharInfo extends Component<charInfoProps, charInfoState> {
             .catch(this.onError)
     }
 
-    private onCharLoaded(char: any) {
+    private onCharLoaded = (char: any) => {
         this.setState({
             char,
-            loading: true
+            loading: false
         });
     }
 
-    private onCharLoading() {
+    private onCharLoading = () => {
         this.setState({
             loading: true
         });
     }
 
-    private onError(e: any) {
+    private onError = (e: any) => {
         console.log(e);
 
         this.setState({
@@ -83,63 +90,45 @@ class CharInfo extends Component<charInfoProps, charInfoState> {
                 {spinner}
                 {content}
             </div>
-        )
+        );
     }
 }
 
-// TODO
-const View = ({char}: any) => {
+const View = ({char}: {char: character}) => {
+    const {name, description, thumbnail, homepage, wiki, comics} = char;
+
     return (
         <>
             <div className="char__basics">
-                <img src={thor} alt="abyss"/>
+                <img src={thumbnail} alt="abyss"/>
                 <div>
-                    <div className="char__info-name">thor</div>
+                    <div className="char__info-name">{name}</div>
                     <div className="char__btns">
-                        <a href="/#" className="button button__main">
+                        <a href={homepage} className="button button__main">
                             <div className="inner">homepage</div>
                         </a>
-                        <a href="/#" className="button button__secondary">
+                        <a href={wiki} className="button button__secondary">
                             <div className="inner">Wiki</div>
                         </a>
                     </div>
                 </div>
             </div>
             <div className="char__descr">
-                In Norse mythology, Loki is a god or jötunn (or both). Loki is the son of Fárbauti and Laufey, and the brother of Helblindi and Býleistr. By the jötunn Angrboða, Loki is the father of Hel, the wolf Fenrir, and the world serpent Jörmungandr. By Sigyn, Loki is the father of Nari and/or Narfi and with the stallion Svaðilfari as the father, Loki gave birth—in the form of a mare—to the eight-legged horse Sleipnir. In addition, Loki is referred to as the father of Váli in the Prose Edda.
+                {description}
             </div>
             <div className="char__comics">Comics:</div>
             <ul className="char__comics-list">
-                <li className="char__comics-item">
-                    All-Winners Squad: Band of Heroes (2011) #3
-                </li>
-                <li className="char__comics-item">
-                    Alpha Flight (1983) #50
-                </li>
-                <li className="char__comics-item">
-                    Amazing Spider-Man (1999) #503
-                </li>
-                <li className="char__comics-item">
-                    Amazing Spider-Man (1999) #504
-                </li>
-                <li className="char__comics-item">
-                    AMAZING SPIDER-MAN VOL. 7: BOOK OF EZEKIEL TPB (Trade Paperback)
-                </li>
-                <li className="char__comics-item">
-                    Amazing-Spider-Man: Worldwide Vol. 8 (Trade Paperback)
-                </li>
-                <li className="char__comics-item">
-                    Asgardians Of The Galaxy Vol. 2: War Of The Realms (Trade Paperback)
-                </li>
-                <li className="char__comics-item">
-                    Vengeance (2011) #4
-                </li>
-                <li className="char__comics-item">
-                    Avengers (1963) #1
-                </li>
-                <li className="char__comics-item">
-                    Avengers (1996) #1
-                </li>
+                {
+                    comics
+                        ? comics.items.map((item, i) => {
+                            return (
+                                <li className="char__comics-item" key={i}>
+                                    {item.name}
+                                </li>
+                            )
+                        })
+                        : null
+                }
             </ul>
         </>
     );
