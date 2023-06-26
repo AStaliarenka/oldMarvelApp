c74e4ec3b824d24610ed07f169465ddbad0d50b0
Aleksey Stolyarenko <astoliarenko@xbsoftware.by>
2023-06-26 17:09:11 +0300

    refactor: changed marvelService

--- src/services/MarvelService.ts
+++ src/services/MarvelService.ts
@@ -2,40 +2,18 @@ import keys from "./keys";
 
 import { marvelAPI } from "./marvelApi";
 
-import { Character } from "marvel-ts/dist/types";
+import { useHttp } from "../hooks/http.hook";
+
+import { Character} from "marvel-ts/dist/types";
 
 import { character as myCharacter } from "../components/interfaces/character";
+
+let _charactersTotal: number | undefined = 0;
 
 class MarvelService {
 	private _apiBase = 'https://gateway.marvel.com:443/v1/public/';
 	private _apiKeyParam = `apikey=${keys.public}`;
 	private _charactersTotal: undefined | number;
-
-	public getResource = async (url: string) => {
-		const res = await fetch(url);
-
-		if (!res.ok) {
-			throw new Error(`Could not fetch ${url}, status: ${res.status}`)
-		}
-
-		return res.json();
-	}
-
-	public getAllCharacters = (additionalParams?: {limit?: number, offset?: number}) => {
-		let url = `${this._apiBase}characters?${this._apiKeyParam}`;
-
-		if (additionalParams) {
-			const keys = Object.keys(additionalParams) as Array<keyof typeof additionalParams>;
-
-			if (keys.length) {
-				keys.forEach((key) => {
-					url += `&${key}=${additionalParams[key]}`;
-				});
-			}
-		}
-
-		return this.getResource(url);
-	}
 
 	public getCharacterById = async (id: number) => {
 		const res = await marvelAPI.getCharacterById(id);
@@ -82,4 +60,61 @@ class MarvelService {
 	}
 }
 
+const useMarvelService = () => {
+	const {loading, requestFunc, error} = useHttp();
+
+	const getCharacters = async (offset: number = 210, limit?: number) => {
+		const func = marvelAPI.getCharacters;
+		const res = await requestFunc(func, {offset, limit: limit ? limit : 9}) as Awaited<ReturnType<typeof func>>;
+
+		if (res.code === 200 && res.data?.results) {
+			if (!_charactersTotal) {
+				_charactersTotal = res.data.total;
+			}
+
+			return res.data.results.map(character => {
+				return {
+					id: character.id,
+					name: character.name,
+					thumbnail: character.thumbnail ? `${character.thumbnail.path}.${character.thumbnail.extension}` : '',
+				};
+			});
+		}
+	}
+
+	const transformCharacterData = (character: Character): myCharacter => {
+		return {
+			name: character.name || 'Unknown hero',
+			description: character.description || 'no description about character',
+			thumbnail: character.thumbnail ? `${character.thumbnail.path}.${character.thumbnail.extension}` : '',
+			homepage: character.urls ? (character.urls[0].url || '/#') : '/#',
+			wiki: character.urls ? (character.urls[1].url || '/#') : '/#',
+			comics: character.comics
+		};
+	}
+
+	const getCharactersTotalCount = () => {
+		return _charactersTotal ? _charactersTotal : 0;
+	}
+
+	const getCharacterById = async (id: number) => {
+		const func = marvelAPI.getCharacterById;
+		const res = await requestFunc(func.bind(marvelAPI), id) as Awaited<ReturnType<typeof func>>;
+
+		if (res.data?.results) {
+			return transformCharacterData(res.data.results[0]);
+		}
+	}
+
+	return {
+		loading,
+		error,
+		getCharacterById,
+		getCharacters,
+		getCharactersTotalCount
+	}
+}
+
 export default MarvelService;
+
+export {useMarvelService};
