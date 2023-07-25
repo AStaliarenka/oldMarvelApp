f0f058e6455b86a26c663434b21462b0b65eb08e
Aleksey Stolyarenko <astoliarenko@xbsoftware.by>
2023-07-25 13:01:51 +0300

    feat: helpers

---
+++ src/helpers/common.ts
@@ -0,0 +1,8 @@
+import { useEffect } from "react";
+
+const useDidMount = (func: React.EffectCallback) => {
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  useEffect(func, []);
+}
+
+export {useDidMount};
