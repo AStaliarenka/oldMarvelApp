cca81e9906d641ac714195f9ce69ac95c92059c8
Aleksey Stolyarenko <astoliarenko@xbsoftware.by>
2023-08-01 12:51:53 +0300

    feat: testForm

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -4,7 +4,7 @@ import AppHeader from "../appHeader/AppHeader";
 
 import ErrorBoundary from '../errorBoundary/ErrorBoundary';
 
-import { MainPage, ComicsPage, Page404, SingleComicPage } from '../pages';
+import { MainPage, ComicsPage, Page404, SingleComicPage, TestFormPage } from '../pages';
 
 const App  = () => {
     const errFallback = <p>Something went wrong</p>;
@@ -28,6 +28,12 @@ const App  = () => {
                         <Route path='comics/:comicId' element={
                             <ErrorBoundary fallback={errFallback}>
                                 <SingleComicPage/>
+                            </ErrorBoundary>
+                        }>
+                        </Route>
+                        <Route path='testForm' element={
+                            <ErrorBoundary fallback={errFallback}>
+                                <TestFormPage/>
                             </ErrorBoundary>
                         }>
                         </Route>
--- src/components/pages/index.ts
+++ src/components/pages/index.ts
@@ -2,10 +2,12 @@ import MainPage from "./main";
 import ComicsPage from "./comics";
 import Page404 from "./404";
 import SingleComicPage from "./singleComic";
+import TestFormPage from "./testForm";
 
 export {
     MainPage,
     ComicsPage,
     Page404,
-    SingleComicPage
+    SingleComicPage,
+    TestFormPage
 }
