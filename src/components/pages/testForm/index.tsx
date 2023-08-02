e79dcdb300bb458ce18c8c4c0f5581d21f7a230c
Aleksey Stolyarenko <astoliarenko@xbsoftware.by>
2023-08-02 16:24:36 +0300

    feat: test form

--- src/components/pages/testForm/index.tsx
+++ src/components/pages/testForm/index.tsx
@@ -1,29 +1,67 @@
 import { useState } from "react";
 import { useForm } from "react-hook-form";
 
+import AppContainer from "../../appContainer";
+
 import "./style.scss";
 
+type TestForm = {
+  firstName: string;
+  category: string;
+  aboutYou: string;
+}
+
+enum TestFormInputNames {
+  firstName = 'firstName',
+  category = 'category',
+  aboutYou = 'aboutYou'
+}
+
 export default function TestFormPage() {
-  const { register, handleSubmit } = useForm();
+  const { register, handleSubmit, setValue, clearErrors, formState: {errors} } = useForm<TestForm>({
+    defaultValues: {
+      [TestFormInputNames.firstName]: 'ALEXIUS',
+      [TestFormInputNames.aboutYou]: 'genius'
+    }
+  });
+
+  // const submit: SubmitHandler<TestForm> = data: any => console.log('data', data);
   const [data, setData] = useState("");
+
+  const onTestButtonClick = () => {
+    setValue(TestFormInputNames.firstName, "Maxim");
+    setValue(TestFormInputNames.aboutYou, "Not a genius");
+    setValue(TestFormInputNames.category, "B");
+  };
 
   const form = (
     <form className="testForm"  onSubmit={handleSubmit((data) => setData(JSON.stringify(data)))}>
-      <input {...register("firstName")} placeholder="First name" />
-      <select {...register("category", { required: true })}>
-        <option value="">Select...</option>
-        <option value="A">Option A</option>
-        <option value="B">Option B</option>
-      </select>
-      <textarea {...register("aboutYou")} placeholder="About you" />
+      
+
+      <div className="testForm__inputs">
+        <input {...register(TestFormInputNames.firstName)} placeholder="First name" />
+        <select {...register(TestFormInputNames.category, { required: true })} aria-invalid={errors.category ? true : false}>
+          <option value="">Select...</option>
+          <option value="A">Option A</option>
+          <option value="B">Option B</option>
+        </select>
+        <textarea {...register(TestFormInputNames.aboutYou)} placeholder="About you" />
+      </div>
       <p>{data}</p>
-      <input type="submit" />
+      <div className="testForm__buttons">
+        <button>Submit</button>
+        <button type="button" onClick={() => {onTestButtonClick()}}>Test BUTTON</button>
+        <button type="button" onClick={() => {clearErrors()}}>CLEAR errors</button>
+      </div>
+      
     </form>
   );
 
   return (
     <div className="wrapper">
-      {form}
+      <AppContainer>
+        {form}
+      </AppContainer>
     </div>
   );
 }
