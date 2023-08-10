09ae799fb28a8d6686986813fb8ee81b775fd54f
Aleksey Stolyarenko <astoliarenko@xbsoftware.by>
2023-08-10 17:33:50 +0300

    fix: form

--- src/components/pages/testForm/index.tsx
+++ src/components/pages/testForm/index.tsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { useForm } from "react-hook-form";
+import {get as getDataFromLs} from "local-storage";
 
 import AppContainer from "../../appContainer";
 import MarvelButton from "../../marvelButton";
@@ -13,6 +14,11 @@ type TestForm = {
   aboutYou: string;
 }
 
+type UserInfo = {
+  firstName: string;
+  lastName: string;
+}
+
 enum TestFormInputNames {
   firstName = 'firstName',
   lastName = 'lastName',
@@ -22,7 +28,6 @@ enum TestFormInputNames {
 }
 
 const selectOptions: Record<string, string> = {
-  empty: 'Select...',
   bumer: 'Bumer',
   zumer: 'Zumer',
   singer: 'Singer',
@@ -38,28 +43,58 @@ export default function TestFormPage() {
     }
   });
 
-  // const submit: SubmitHandler<TestForm> = data: any => console.log('data', data);
   const [data, setData] = useState("");
 
   const onTestButtonClick = () => {
     setValue(TestFormInputNames.firstName, "Alex");
     setValue(TestFormInputNames.lastName, "Stoliarenko");
     setValue(TestFormInputNames.aboutYou, "Not a genius");
-    setValue(TestFormInputNames.category, "B");
+    setValue(TestFormInputNames.category, Object.keys(selectOptions)[0]);
   };
 
+  const onValidSubmit = (data: TestForm) => {
+    const currenUser = getDataFromLs<UserInfo>('user');
+
+    if (currenUser && data.firstName === 'Alex' && data.lastName === 'Stoliarenko') {
+      alert('success');
+    }
+    else {
+      setData(JSON.stringify(data));
+    }
+  };
+
+  const onInvalidSubmit = (...arrM: any[]) => {
+    console.log('arrM', arrM);
+  }
+
   const form = (
-    <form className="testForm"  onSubmit={handleSubmit((data) => setData(JSON.stringify(data)))}>
+    <form className="testForm"  onSubmit={handleSubmit(onValidSubmit, onInvalidSubmit)}>
       <div className="testForm__inputs">
-        <input {...register(TestFormInputNames.firstName)} placeholder="First name" />
-        <input {...register(TestFormInputNames.lastName)} placeholder="Last name" />
-        <select {...register(TestFormInputNames.category, { required: true })} aria-invalid={errors.category ? true : false}>
+        <input {...register(TestFormInputNames.firstName, { maxLength: 20 })} placeholder="First name" aria-invalid={errors.firstName ? "true" : "false"}/>
+        {
+          errors.firstName && errors.firstName.type === "maxLength" && (
+            <span role="alert">Max length exceeded</span>
+          )
+        }
+        <input {...register(TestFormInputNames.lastName, { maxLength: 20 })} placeholder="Last name" aria-invalid={errors.lastName ? "true" : "false"}/>
+        {
+          errors.lastName && errors.lastName.type === "maxLength" && (
+            <span role="alert">Max length exceeded</span>
+          )
+        }
+        {/* React does not support the selected attribute on <option>. Instead, pass this option’s value to the parent
+        <select defaultValue> for an uncontrolled select box, or <select value> for a controlled select. */}
+        <select {...register(TestFormInputNames.category, { required: true })} defaultValue={selectOptions.empty} aria-invalid={errors.category ? "true" : "false"}>
           {
-            Object.keys(selectOptions).map((key, index) => {
-              return <option key={index} value={key}>{selectOptions[key]}</option>;
-            })
+            [
+              <option key='empty' value=''>Select...</option>,
+              ...Object.keys(selectOptions).map((key, index) => {
+                return <option key={index} value={key}>{selectOptions[key]}</option>;
+              })
+            ]
           }
         </select>
+        {errors.category && errors.category.type === "required" && <span>This is required</span>}
         <textarea {...register(TestFormInputNames.aboutYou)} placeholder="About you" />
       </div>
       <p>{data}</p>
