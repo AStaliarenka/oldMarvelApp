61cc8605d23db8082e574a699061e48ecb3fbe0d
Alex Stoliarenko <alexstoliarenko.work@gmail.com>
2024-03-01 16:33:48 +0300

    feat: added store

--- src/app/store/store.ts
+++ src/app/store/store.ts
@@ -2,13 +2,10 @@ import { configureStore} from '@reduxjs/toolkit';
 
 import { authReducer } from '../reducers/auth';
 
-// TODO:
-const charactersReducer = () => {};
 
 const store = configureStore({
     reducer: {
-        auth: authReducer,
-        characters: charactersReducer
+        auth: authReducer
     },
 });
 
@@ -17,3 +14,5 @@ export type RootState = ReturnType<typeof store.getState>;
 
 // Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
 export type AppDispatch = typeof store.dispatch;
+
+export default store;
--- src/index.tsx
+++ src/index.tsx
@@ -2,13 +2,17 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './components/app/App';
 import './style/style.scss';
+import { Provider } from 'react-redux';
+import store from './app/store/store';
 
 const container = document.getElementById('root') as HTMLElement;
 const root = createRoot(container);
 
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
 
