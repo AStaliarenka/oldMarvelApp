a7897ed3b7085ddf56ed0cbcce09dc34aa1b3d84
Alex Stoliarenko <alexstoliarenko.work@gmail.com>
2024-10-22 15:34:17 +0300

    feat: add skeleton theme toggle

--- src/components/appHeader/AppHeader.tsx
+++ src/components/appHeader/AppHeader.tsx
@@ -6,21 +6,32 @@ const AppHeader = () => {
     const toggleActive = ({isActive}: {isActive: boolean}) => isActive ? "active" : "";
 
     return (
-        <header className="app__header app-container">
-            <h1 className="app__title">
-                <a href="/#">
-                    <span>Marvel</span> information portal
-                </a>
-            </h1>
-            <nav className="app__menu">
-                <ul>
-                    <li><NavLink to="/" className={toggleActive}>Characters</NavLink></li>
-                    /
-                    <li><NavLink to="/comics" className={toggleActive}>Comics</NavLink></li>
-                    /
-                    <li><NavLink to="/testForm" className={toggleActive}>Test Form</NavLink></li>
-                </ul>
-            </nav>
+        <header className="app__header mainHeader app-container">
+            <div className='mainHeader__topRow'>
+                <div className='mainHeader__themeToggle themeToggle'>
+                    <span className='themeToggle__label'>Theme</span>
+                    <label className = "switch">
+                        <input type="checkbox"></input>
+                        <span className="slider round"></span>
+                    </label>
+                </div>
+            </div>
+            <div className="mainHeader__bottomRow">
+                <h1 className="app__title">
+                    <a href="/#">
+                        <span>Marvel</span> information portal
+                    </a>
+                </h1>
+                <nav className="app__menu">
+                    <ul>
+                        <li><NavLink to="/" className={toggleActive}>Characters</NavLink></li>
+                        /
+                        <li><NavLink to="/comics" className={toggleActive}>Comics</NavLink></li>
+                        /
+                        <li><NavLink to="/testForm" className={toggleActive}>Test Form</NavLink></li>
+                    </ul>
+                </nav>
+            </div>
         </header>
     )
 }
