735f4ae08120ccd5a15d7adf26bd47523e1ac6b9
Alex Stoliarenko <alexstoliarenko.work@gmail.com>
2024-10-25 14:23:21 +0300

    fix: AppHeader

--- src/components/appHeader/AppHeader.tsx
+++ src/components/appHeader/AppHeader.tsx
@@ -4,30 +4,34 @@ import Theme from '../theme';
 
 import { NavLink } from 'react-router-dom';
 
+import AppContainer from '../appContainer';
+
 const AppHeader = () => {
     const toggleActive = ({isActive}: {isActive: boolean}) => isActive ? "active" : "";
 
     return (
-        <header className="app__header mainHeader app-container">
-            <div className='mainHeader__topRow'>
-                <Theme/>
-            </div>
-            <div className="mainHeader__bottomRow">
-                <h1 className="app__title">
-                    <a href="/#">
-                        <span>Marvel</span> information portal
-                    </a>
-                </h1>
-                <nav className="app__menu">
-                    <ul>
-                        <li><NavLink to="/" className={toggleActive}>Characters</NavLink></li>
-                        /
-                        <li><NavLink to="/comics" className={toggleActive}>Comics</NavLink></li>
-                        /
-                        <li><NavLink to="/testForm" className={toggleActive}>Test Form</NavLink></li>
-                    </ul>
-                </nav>
-            </div>
+        <header className="app__header mainHeader">
+            <AppContainer>
+                <div className='mainHeader__topRow'>
+                    <Theme/>
+                </div>
+                <div className="mainHeader__bottomRow">
+                    <h1 className="app__title">
+                        <a href="/#">
+                            <span>Marvel</span> information portal
+                        </a>
+                    </h1>
+                    <nav className="app__menu">
+                        <ul>
+                            <li><NavLink to="/" className={toggleActive}>Characters</NavLink></li>
+                            /
+                            <li><NavLink to="/comics" className={toggleActive}>Comics</NavLink></li>
+                            /
+                            <li><NavLink to="/testForm" className={toggleActive}>Test Form</NavLink></li>
+                        </ul>
+                    </nav>
+                </div>
+            </AppContainer>
         </header>
     )
 }
