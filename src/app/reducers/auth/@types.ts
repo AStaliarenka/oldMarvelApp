7682e97344f30d6fd2e1ff15f00fab9b049ba07c
Alex Stoliarenko <alexstoliarenko.work@gmail.com>
2024-11-12 11:56:35 +0300

    fix: commented unused code

--- src/app/reducers/auth/@types.ts
+++ src/app/reducers/auth/@types.ts
@@ -1,7 +1,7 @@
-export enum AuthActionTypes {
-    login = "login",
-    logout = "logout"
-}
+// export enum AuthActionTypes {
+//     login = "login",
+//     logout = "logout"
+// }
 
 export type AuthState = {
     isSignedIn: boolean;
@@ -9,13 +9,13 @@ export type AuthState = {
     error: null | string;
 };
 
-type LoginAuthAction = {
-    type: AuthActionTypes.login;
-    payload: any;
-}
+// type LoginAuthAction = {
+//     type: AuthActionTypes.login;
+//     payload: any;
+// }
 
-type LogoutAuthAction = {
-    type: AuthActionTypes.logout;
-}
+// type LogoutAuthAction = {
+//     type: AuthActionTypes.logout;
+// }
 
-export type AuthAction = LoginAuthAction | LogoutAuthAction;
+// export type AuthAction = LoginAuthAction | LogoutAuthAction;
